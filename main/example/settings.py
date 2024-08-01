d04e39a67211eb42ce9d8743a03fb1f51194f1c2
Yiğit Burak <yigitburak2005@gmail.com>
2024-08-01 23:44:10 +0300

    More config

--- main/example/settings.py
+++ main/example/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'example.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'host.docker.internal',
+        'HOST': '127.0.0.1',
         'NAME': 'postgres',
-        'USER': 'notes_db',
+        'USER': 'postgres',
         'PASSWORD': '0000',
     }
 }
--- main/run.sh
+++ main/run.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate
 python manage.py collectstatic --link --no-input
-python manage.py runserver --insecure 0.0.0.0:80
+python manage.py runserver --insecure 127.0.0.1:8080
